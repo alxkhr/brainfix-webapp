cfe94a72d88a46c8c4ad78420f532fa6cc616d64
Alexander Kehr <alex@alexanderkehr.de>
2019-10-14 21:55:03 +0200

    wipe

--- src/note/note.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Note {
-  uuid: string;
-  content: string;
-  dateModified: string;
-  dateCreated: string;
-  dateSync: string; // Server Date
-  synchronized: boolean;
-  deleted: boolean;
-}
